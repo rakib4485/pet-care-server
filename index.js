02be353387f2facf3330aba8225f607ba0d5e074
Md Rakibul Islam <mdrakibulislam4485@gmail.com>
2024-05-10 15:39:53 +0600

    doctor added

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ async function run() {
     const appointmentOptionCollection = client.db('petCare').collection('appointmentOption');
     const cartsCollection = client.db('petCare').collection('carts');
     const bookingsCollection = client.db('petCare').collection('bookings')
+    const productOrderCollection = client.db('petCare').collection('productOrders');
+    const sellersCollection = client.db('petCare').collection('sellers');
 
 
     app.get('/products', async (req, res) => {
@@ -64,12 +66,58 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/appointmentOptions', async (req, res) => {
+      await client.connect()
+      const appointmentOption = req.body;
+      console.log(appointmentOption)
+      const query = { email: appointmentOption.email };
+      console.log(query)
+      const already = await usersCollection.findOne(query);
+      console.log(already)
+      if (already) {
+        const result = await appointmentOptionCollection.insertOne(appointmentOption);
+        console.log(result)
+        if (result.acknowledged) {
+          const result1 = await usersCollection.updateOne(
+            { email: appointmentOption.email },
+            {
+              $set: {
+                role: 'doctor'
+              },
+            }
+          );
+          return res.send(result1)
+        }
+      }
+
+      res.send({ acknowledged: false });
+    })
+
     app.get('/appointmentOption/:id', async (req, res) => {
       await client.connect();
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await appointmentOptionCollection.findOne(query);
       res.send(result);
+    });
+
+    app.get('/appointmentSlots/:email', async (req, res) => {
+      await client.connect()
+      const date = req.query.date;
+      const email = req.params.email;
+      const query = { email };
+      const option = await appointmentOptionCollection.findOne(query);
+      //get the booking of provided date
+      const bookingQuery = {
+        appointmentDate: date
+      }
+      const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+      const optionBooked = alreadyBooked.filter(book => book.doctorEmail === option.email);
+      const bookedSlots = optionBooked.map(book => book.slot);
+      const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+      option.slots = remainingSlots;
+      res.send(option);
     });
 
     const tranId = new ObjectId().toString();
@@ -125,9 +173,17 @@ async function run() {
         let GatewayPageURL = apiResponse.GatewayPageURL
         res.send({ url: GatewayPageURL })
         const finalBooking = {
-          booking,
+          appointmentDate: booking.appointmentDate,
           tranjectionId: tranId,
-          paymentStatus: false
+          paymentStatus: false,
+          treatment: booking.treatment,
+          slot: booking.slot,
+          patient: booking.patient,
+          email: booking.email,
+          doctorEmail: booking.doctorEmail,
+          phone: booking.phone,
+          prices: booking.prices,
+          meet: booking.meet
         }
         const result = bookingsCollection.insertOne(finalBooking)
         console.log('Redirecting to: ', GatewayPageURL)
@@ -143,11 +199,20 @@ async function run() {
             },
           }
         );
-        if(result.modifiedCount > 0){
+        console.log(result)
+        if (result.modifiedCount > 0) {
           res.redirect(`http://localhost:3000/appointmentPayment/success/:${req.params.tranId}`)
         }
         return res.send()
       })
+    })
+
+    app.get('/bookings', async (req, res) => {
+      await client.connect();
+      const email = req.query.email;
+      const query = { email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
     })
 
 
@@ -167,6 +232,105 @@ async function run() {
       res.send(result)
     })
 
+    app.post('/productOrder', async (req, res) => {
+      await client.connect();
+      const order = req.body;
+      if (order.paymentType === 'online') {
+        const data = {
+          total_amount: order.price,
+          currency: 'BDT',
+          tran_id: tranId, // use unique tran_id for each api call
+          success_url: `http://localhost:5000/payment/success/${tranId}`,
+          fail_url: 'http://localhost:3030/fail',
+          cancel_url: 'http://localhost:3030/cancel',
+          ipn_url: 'http://localhost:3030/ipn',
+          shipping_method: 'Courier',
+          product_name: 'Computer.',
+          product_category: 'Electronic',
+          product_profile: order.name,
+          cus_name: order.name,
+          cus_email: order.customerEmail,
+          cus_add1: 'Dhaka',
+          cus_add2: 'Dhaka',
+          cus_city: 'Dhaka',
+          cus_state: 'Dhaka',
+          cus_postcode: '1000',
+          cus_country: 'Bangladesh',
+          cus_phone: order.phone,
+          cus_fax: '01711111111',
+          ship_name: 'Customer Name',
+          ship_add1: 'Dhaka',
+          ship_add2: 'Dhaka',
+          ship_city: 'Dhaka',
+          ship_state: 'Dhaka',
+          ship_postcode: 1000,
+          ship_country: 'Bangladesh',
+        };
+        const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+        sslcz.init(data).then(apiResponse => {
+          // Redirect the user to payment gateway
+          let GatewayPageURL = apiResponse.GatewayPageURL
+          res.send({ url: GatewayPageURL })
+          const finalOrder = {
+            name: order.name,
+            price: order.price,
+            city: order.city,
+            street: order.street,
+            products: order.products,
+            paymentType: order.paymentType,
+            customerEmail: order.customerEmail,
+            phone: order.phone,
+            paymentStatus: false,
+            tranjectionId: tranId,
+            orderDate: order.orderDate
+          }
+          const result = productOrderCollection.insertOne(finalOrder)
+          console.log('Redirecting to: ', GatewayPageURL)
+        });
+
+        app.post('/payment/success/:tranId', async (req, res) => {
+          await client.connect();
+          const result = await productOrderCollection.updateOne(
+            { tranjectionId: req.params.tranId },
+            {
+              $set: {
+                paymentStatus: true
+              },
+            }
+          );
+          if (result.modifiedCount > 0) {
+            const del = await cartsCollection.deleteMany({ customerEmail: order.customerEmail });
+            console.log(del)
+            if (del.deletedCount > 0) {
+              res.redirect(`http://localhost:3000/orderPayment/success/:${req.params.tranId}`)
+            }
+
+          }
+          return res.send()
+        })
+      }
+      else {
+        const result = await productOrderCollection.insertOne(order);
+        console.log(result)
+        if (result.acknowledged) {
+          const del = await cartsCollection.deleteMany({ customerEmail: order.customerEmail });
+          console.log(del)
+          if (del.deletedCount > 0) {
+            return res.send({ acknowledged: true });
+          }
+        }
+
+      }
+    });
+
+    app.get('/orders', async (req, res) => {
+      await client.connect()
+      const email = req.query.email;
+      const query = { customerEmail: email };
+      const order = await productOrderCollection.find(query).toArray()
+      res.send(order)
+    })
+
     //user related code
     app.get('/users', async (req, res) => {
       await client.connect();
@@ -178,9 +342,107 @@ async function run() {
     app.post('/users', async (req, res) => {
       await client.connect();
       const user = req.body;
+      const query = { email: user.email };
+      const already = await usersCollection.findOne(query);
+      if (already.length) {
+        return res.send({ acknowledged: false });
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+    app.put('/sellerRequest', async (req, res) => {
+      await client.connect()
+      const seller = req.body;
+      const filter = { email: seller.email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          role: 'sellerRequest',
+          sellerInfo: seller
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    });
+
+    app.get('/sellerRequest', async (req, res) => {
+      await client.connect()
+      const query = { role: 'sellerRequest' };
+      const result = await usersCollection.find(query).toArray()
+      res.send(result);
+    });
+
+    app.get('/sellerInfo', async (req, res) => {
+      await client.connect()
+      const email = req.query.email;
+      const query = { email }
+      const result = await usersCollection.findOne(query);
+      res.send(result);
     })
+
+    app.get('/sellers', async (req, res) => {
+      await client.connect()
+      const query = { role: 'seller' };
+      const result = await usersCollection.find(query).toArray()
+      res.send(result);
+    });
+
+    app.get('/doctors', async (req, res) => {
+      await client.connect()
+      const query = { role: 'doctor' };
+      const result = await usersCollection.find(query).toArray()
+      res.send(result);
+    });
+
+    app.put("/user/update/:role", async (req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      const role = req.params.role;
+      console.log(role)
+      const filter = { email: email }
+      const users = await usersCollection.findOne(filter);
+      const option = { upsert: true };
+      if (role === 'request') {
+        const updatedDoc = {
+          $set: {
+            role: role,
+          },
+        };
+        const result = await usersCollection.updateOne(
+          filter,
+          updatedDoc,
+          option
+        );
+        return res.send({ acknowledged: true });
+      }
+      else if (role === 'confirm') {
+        const updatedDoc = {
+          $set: {
+            role: 'seller',
+          },
+        };
+        const result = await usersCollection.updateOne(
+          filter,
+          updatedDoc,
+          option
+        );
+        return res.send({ acknowledged: true });
+      }
+      else {
+        const updatedDoc = {
+          $set: {
+            role: 'user',
+          },
+        };
+        const result = await usersCollection.updateOne(
+          filter,
+          updatedDoc,
+          option
+        );
+        return res.send({ acknowledged: true });
+      }
+    });
 
   } finally {
     // Ensures that the client will close when you finish/error
