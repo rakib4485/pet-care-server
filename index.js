fed54ca84be0fb83af112e3ab171a332a865f52e
Md Rakibul Islam <mdrakibulislam4485@gmail.com>
2024-06-29 11:29:14 +0600

    doctor delete complete

--- index.js
+++ index.js
@@ -691,7 +691,6 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await appointmentOptionCollection.deleteOne(query);
-      console.log(result)
       res.send(result)
     })
 
